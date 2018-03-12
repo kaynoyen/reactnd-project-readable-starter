deec07e7fa998e58eed2f58670527179dcaad4c8
Kay Noyen <kaynoyen@gmail.com>
2018-03-12 15:29:44 +0100

    refactor: removed css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,42 +26,14 @@ function Post ({detail}) {
   
 
   return (
+    <div>
+      <h2>{title}</h2>
+      <h3>{author}</h3>
+      <p>{body}</p>
+      <hr/>
+    </div>
 
-    <div className="create-post-box">
-          <div>
-        <form>
-          <div>
-                <select className="select-input">
-                  <option value="" disabled>Choose category</option>  
-                          <option value="volvo">{category}</option>
-                          <option value="saab">Saab</option>
-                          <option value="mercedes">Mercedes</option>
-                          <option value="audi">Audi</option>
-                      </select>
-            <input className="text-input" type="text" name='title' placeholder='Title' defaultValue={title}/>
-            <input className="text-input" type="text" name='author' placeholder='Email' defaultValue={author}/>
-                
-                <textarea 
-                  className="text-input" 
-                  type="text" 
-                  name='post' 
-                  placeholder="Post" 
-                  cols="30" 
-                  rows="5"
-                  defaultValue={body}
-                  readOnly/>
-              </div>
-              <div>
-                <button className="submit-button">Edit</button>
-
-                  <button className="back-button">Delete</button>
-  
-          </div>
-        </form> 
-      </div>
-        </div>
-
-    )
+  )
 }
 
 function ListPosts ({posts, isFetching}) {
@@ -91,10 +63,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Readable</h1>
-          <NavBar className="nav-bar" categories={categories} isFetching={categories.isFetching}/>
         </header>
           
-          
+          <NavBar categories={categories} isFetching={categories.isFetching}/>
           <ListPosts posts={posts} isFetching={posts.isFetching}/>
       </div>
     );
