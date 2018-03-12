8b785ba272a09afc11df22671c41fc506076b323
Kay Noyen <kaynoyen@gmail.com>
2018-03-12 15:19:26 +0100

    refactor: put functional components into app.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,76 @@ import './App.css'
 import { connect } from 'react-redux'
 import { fetchCategories, fetchPosts } from './actions'
 
-import NavBar from './components/NavBar'
+import { Route, Link } from 'react-router-dom'
 
-import ListPosts from './components/ListPosts'
+
+function NavBar ({categories, isFetching}) {
+
+  return (
+    <ul className="nav-bar">
+        {isFetching ? 
+            <p> LOADING... </p> : 
+              categories.items.map(cat => (
+                <Link key= {cat.path} to={cat.path}>
+                  <li key={cat.name} >{cat.name}</li>
+                </Link>
+              ))}
+      </ul>
+    )
+}
+
+function Post ({detail}) {
+  const { id, timestamp, title, body, author, category} = detail
+  
+
+  return (
+
+    <div className="create-post-box">
+          <div>
+        <form>
+          <div>
+                <select className="select-input">
+                  <option value="" disabled>Choose category</option>  
+                          <option value="volvo">{category}</option>
+                          <option value="saab">Saab</option>
+                          <option value="mercedes">Mercedes</option>
+                          <option value="audi">Audi</option>
+                      </select>
+            <input className="text-input" type="text" name='title' placeholder='Title' defaultValue={title}/>
+            <input className="text-input" type="text" name='author' placeholder='Email' defaultValue={author}/>
+                
+                <textarea 
+                  className="text-input" 
+                  type="text" 
+                  name='post' 
+                  placeholder="Post" 
+                  cols="30" 
+                  rows="5"
+                  defaultValue={body}
+                  readOnly/>
+              </div>
+              <div>
+                <button className="submit-button">Edit</button>
+
+                  <button className="back-button">Delete</button>
+  
+          </div>
+        </form> 
+      </div>
+        </div>
+
+    )
+}
+
+function ListPosts ({posts, isFetching}) {
+
+  return (
+    isFetching ? <p> LOADING... </p> :
+    posts.items.map(post => (
+      <Post key={post.id} detail={post}/>
+      ))
+    )
+}
 
 class App extends Component {
 
@@ -18,7 +85,6 @@ class App extends Component {
   render() {
 
     const { categories, posts } = this.props
-
     console.log(posts)
 
     return (
